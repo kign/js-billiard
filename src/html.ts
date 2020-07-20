20b98d0bc9cf3b54eeb9199a22fd30f420462647
Konstantin Ignatiev <kignatiev@tripadvisor.com>
2020-07-20 18:48:45 -0400

    Added decel, speed

--- src/html.ts
+++ src/html.ts
@@ -1,11 +1,15 @@
 interface htmlElements {
 	readonly canvas: HTMLCanvasElement;
 	readonly slider: HTMLElement;
+	readonly decel: HTMLInputElement;
+	readonly speed: HTMLInputElement;
 }
 
 export const html : htmlElements = {
 	canvas: document.getElementById('canvas')! as HTMLCanvasElement,
-	slider: document.getElementById('slider')!
+	slider: document.getElementById('slider')!,
+	decel: document.getElementById('decel')! as HTMLInputElement,
+	speed: document.getElementById('speed')! as HTMLInputElement
 }
 
 //console.log("Loaded html.ts @" + (new Date()));
