5950811201ca1e9336f0cd5287c68afec00dc8b5
Konstantin Ignatiev <kignatiev@tripadvisor.com>
2020-07-23 17:44:11 -0400

    Added dbglog()

--- src/html.ts
+++ src/html.ts
@@ -3,13 +3,24 @@ interface htmlElements {
 	readonly slider: HTMLElement;
 	readonly decel: HTMLInputElement;
 	readonly speed: HTMLInputElement;
+	readonly dbg?: HTMLTableElement;
 }
 
 export const html : htmlElements = {
 	canvas: document.getElementById('canvas')! as HTMLCanvasElement,
 	slider: document.getElementById('slider')!,
 	decel: document.getElementById('decel')! as HTMLInputElement,
-	speed: document.getElementById('speed')! as HTMLInputElement
+	speed: document.getElementById('speed')! as HTMLInputElement,
+	dbg: document.getElementById('dbg') as HTMLTableElement
 }
 
 //console.log("Loaded html.ts @" + (new Date()));
+
+export function dbglog(msg: string) :void {
+	if (html.dbg != undefined) {
+		const tr = html.dbg.insertRow(0);
+		const td = tr.insertCell(0);
+
+		td.innerHTML = msg;
+	}
+}
